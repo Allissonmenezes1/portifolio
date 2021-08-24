f7e661d07c083a90405c489bd21534bb37afdc8b
allissonmenezes1 <allissonctt1@outlook.com>
2021-08-24 14:54:26 -0300

    arquivos novos 24/08/2021

---
+++ script.js
@@ -0,0 +1,6 @@
+var instagram = document.querySelector(".instagram");
+
+instagram.addEventListener("Click", function(){
+     document.querySelector(".container").classList.toggle("show-menu");
+
+});
